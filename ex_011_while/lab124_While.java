bee6749a633ba0d595fa2324da365d92696f492f
shagufta219 <shaguftasabaansari@gmail.com>
2025-06-20 07:39:27 +0530

    20th June

---
+++ ex_011_while/lab124_While.java
@@ -0,0 +1,11 @@
+package ex_011_while;
+
+public class lab124_While {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
